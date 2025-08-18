bccc4be2e215812589e84bc5bf89afd30b05215d
tsx <miladtsx@gmail.com>
2025-08-18 20:27:40 +0330

    fix(code): add try catch

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
 from src.detect_vehicle import run_plate_detection
 
 if __name__ == "__main__":
-    run_plate_detection()
+    try:
+        run_plate_detection()
+    except Exception as e:
+        print(f"Error: {e}")
