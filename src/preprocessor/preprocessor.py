4eeb0483e312449065e79aaf45c5fe63c2474c9e
tsx <miladtsx@gmail.com>
2025-10-29 08:46:23 +0330

    remove unused

--- src/preprocessor/preprocessor.py
+++ src/preprocessor/preprocessor.py
@@ -1,9 +1,6 @@
-import os
 import cv2
 import numpy as np
 from typing import Tuple
-from src.common_utils.config import Config
-from src.common_utils.resource_path import get_data_path
 
 
 class PlatePreprocessor:
