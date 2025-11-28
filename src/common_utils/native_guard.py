af5659754b035e89cbe52456ad9ffdc4ac81f0bb
tsx <miladtsx@gmail.com>
2025-11-28 12:46:00 +0330

    chore(debug): remove dev

--- src/common_utils/native_guard.py
+++ src/common_utils/native_guard.py
@@ -15,7 +15,6 @@ _thread_local = threading.local()
 
 
 def debugger_present() -> bool:
-    return False #TODO for development purposes
     tracer = sys.gettrace()
     if tracer is not None:
         return True
